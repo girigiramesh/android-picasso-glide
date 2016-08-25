27d2792eb8d6b756b982a0465ad388ad6869d843
ramesh <hari.kvana@gmail.com>
2016-08-25 18:21:06 +0530

    first commit

---
+++ app/src/main/java/com/picasso/MainActivity.java
@@ -0,0 +1,59 @@
+package com.picasso;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView image, imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with Your Own Action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //==================picasso======================
+
+        image = (ImageView) findViewById(R.id.image);
+        String picasso_url = "http://www.cfau-pd.net/images/android-wallpapers-hd/android-wallpapers-hd-4.jpg";
+
+        Picasso.with(image.getContext())
+                .load(picasso_url)
+                .placeholder(R.drawable.team)
+//              .fit()
+                .resize(150, 50)//resizing the image
+                .centerCrop()
+                .into(image);
+
+        //==================glide==========================
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        String glide_url = "http://inthecheesefactory.com/uploads/source/glidepicasso/cover.jpg";
+
+        Glide.with(imageView.getContext())
+                .load(glide_url)
+                .override(150, 50)//resizing the image
+                .into(imageView);
+    }
+}
